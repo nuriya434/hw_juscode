8ff8a3540c558094f029406a2a0a52e098d02f8e
nuriya <nuriya434@mail.ru>
2023-12-03 10:53:59 +0600

    Pandas

--- dataset.py
+++ dataset.py
@@ -1,12 +1,61 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
-# Загрузите данные
-data = pd.read_csv("C:\Users\Nuriya\Downloads\archive\complete.csv")
+def dict_sort(my_dict):
+    keys = []
+    values = []
+    my_dict = sorted(my_dict.items(), key=lambda x:x[1], reverse=True)
+    for k, v in my_dict:
+        keys.append(k)
+        values.append(v)
+    return (keys,values)
 
-# Постройте гистограмму распределения наблюдений по времени
-plt.hist(data["date_time"])
-plt.xlabel("Время")
-plt.ylabel("Количество наблюдений")
+def getColors(n):
+    COLORS = []
+    cm = plt.cm.get_cmap('hsv', n)
+    for i in np.arange(n):
+        COLORS.append(cm(i))
+    return COLORS
+
+df = pd.read_csv('C:\\Users\\Nuriya\\Downloads\\archive\\scrubbed.csv', escapechar='`', low_memory=False)
+
+PLOT_LABEL_FONT_SIZE = 12
+country_count = pd.value_counts(df['country'].values, sort=True)
+country_count_keys, country_count_values = dict_sort(dict(country_count))    
+TOP_COUNTRY = len(country_count_keys)
+plt.title('Страны, где больше всего наблюдений', fontsize=PLOT_LABEL_FONT_SIZE)
+plt.bar(np.arange(TOP_COUNTRY), country_count_values, color=getColors(TOP_COUNTRY))
+plt.xticks(np.arange(TOP_COUNTRY), country_count_keys, rotation=0, fontsize=12)
+plt.yticks(fontsize=PLOT_LABEL_FONT_SIZE)
+plt.ylabel('Количество наблюдений', fontsize=PLOT_LABEL_FONT_SIZE)
+plt.show()
+
+MONTH_COUNT = [0,0,0,0,0,0,0,0,0,0,0,0]
+MONTH_LABEL = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+    'Июль', 'Август', 'Сентябрь' ,'Октябрь' ,'Ноябрь' ,'Декабрь']
+
+for i in df['datetime']:
+    m,d,y_t =  i.split('/')
+    MONTH_COUNT[int(m)-1] = MONTH_COUNT[int(m)-1] + 1
+
+plt.bar(np.arange(12), MONTH_COUNT, color=getColors(12))
+plt.xticks(np.arange(12), MONTH_LABEL, rotation=90, fontsize=PLOT_LABEL_FONT_SIZE)
+plt.ylabel('Частота появления', fontsize=PLOT_LABEL_FONT_SIZE)
+plt.yticks(fontsize=PLOT_LABEL_FONT_SIZE)
+plt.title('Частота появления объектов по месяцам', fontsize=PLOT_LABEL_FONT_SIZE)
+plt.show()
+
+shapes_type_count = pd.value_counts(df['shape'].values)
+shapes_type_count_keys, shapes_type_count_values = dict_sort(dict(shapes_type_count))
+OBJECT_COUNT = len(shapes_type_count_keys)
+
+plt.title('Типы объектов', fontsize=PLOT_LABEL_FONT_SIZE)
+bar = plt.bar(np.arange(OBJECT_COUNT), shapes_type_count_values, color=getColors(OBJECT_COUNT))
+plt.xticks(np.arange(OBJECT_COUNT), shapes_type_count_keys, rotation=90, fontsize=PLOT_LABEL_FONT_SIZE)
+plt.yticks(fontsize=PLOT_LABEL_FONT_SIZE)
+plt.ylabel('Сколько раз видели', fontsize=PLOT_LABEL_FONT_SIZE)
 plt.show()
 
